cfbcd063d801f1eba09f059a951696fdf8c220dc
peaceGloriole <dmtrpwlw@icloud.com>
2023-11-25 16:32:20 +0200

    100/100

--- heroes.js
+++ heroes.js
@@ -5,80 +5,102 @@ function solve(input) {
 
   for (let i = 0; i < numParty; i++) {
     let [name, hp, mp] = input.shift().split(' ');
-    heroes[name] = { name, hp: Math.min(100, Number(hp)), mp: Math.min(200, Number(mp)) };
+    heroes[name] = { name, hp: Number(hp), mp: Number(mp) };
   }
 
-  let match = input.shift();
+  let maxHP = 100;
+  let maxMP = 200;
 
-  while (match != 'End') {
-    let token = match.split(' - ');
-    let command = token[0];
-    let heroName = token[1];
+  let match = input.shift().split(` - `);
+
+  while (match != `End`) {
+    let command = match.shift();
+    let givenHero = match.shift();
+    let digit = Number(match.shift());
 
     switch (command) {
-      case 'TakeDamage':
-        let damageAmount = Number(token[2]);
-        let attacker = token[3];
-        heroes[heroName].hp -= damageAmount;
-        if (heroes[heroName].hp <= 0) {
-          console.log(`${heroName} has been killed by ${attacker}!`);
-          delete heroes[heroName];
+      case `Heal`:
+        let oldHP = heroes[givenHero].hp;
+        heroes[givenHero].hp += digit;
+
+        if (heroes[givenHero].hp > maxHP) {
+          heroes[givenHero].hp = maxHP;
+
+          let healingAmount = maxHP - oldHP;
+          console.log(`${givenHero} healed for ${healingAmount} HP!`);
         } else {
-          console.log(`${heroName} was hit for ${damageAmount} HP by ${attacker} and now has ${heroes[heroName].hp} HP left!`);
+          console.log(`${givenHero} healed for ${digit} HP!`);
         }
+
         break;
-      case 'CastSpell':
-        let spellMP = Number(token[2]);
-        let spellName = token[3];
-        if (heroes[heroName].mp >= spellMP) {
-          heroes[heroName].mp -= spellMP;
-          console.log(`${heroName} has successfully cast ${spellName} and now has ${heroes[heroName].mp} MP!`);
+      case `Recharge`:
+        let oldMP = heroes[givenHero].mp;
+        heroes[givenHero].mp += digit;
+
+        if (heroes[givenHero].mp > maxMP) {
+          heroes[givenHero].mp = maxMP;
+
+          let manaAmount = maxMP - oldMP;
+          console.log(`${givenHero} recharged for ${manaAmount} MP!`);
         } else {
-          console.log(`${heroName} does not have enough MP to cast ${spellName}!`);
+          console.log(`${givenHero} recharged for ${digit} MP!`);
         }
+
         break;
-      case 'Recharge':
-        let rechargeAmount = Number(token[2]);
-        heroes[heroName].mp = Math.min(200, heroes[heroName].mp + rechargeAmount);
-        console.log(`${heroName} recharged for ${rechargeAmount} MP!`);
+      case `TakeDamage`:
+        let enemy = match.shift();
+        heroes[givenHero].hp -= digit;
+
+        if (heroes[givenHero].hp > 0) {
+          console.log(`${givenHero} was hit for ${digit} HP by ${enemy} and now has ${heroes[givenHero].hp} HP left!`);
+        } else {
+          delete heroes[givenHero];
+          console.log(`${givenHero} has been killed by ${enemy}!`);
+        }
+
         break;
-      case 'Heal':
-        let healAmount = Number(token[2]);
-        heroes[heroName].hp = Math.min(100, heroes[heroName].hp + healAmount);
-        console.log(`${heroName} healed for ${healAmount} HP!`);
+      case `CastSpell`:
+        let spellName = match.shift();
+
+        if (heroes[givenHero].mp >= digit) {
+          heroes[givenHero].mp -= digit;
+          console.log(`${givenHero} has successfully cast ${spellName} and now has ${heroes[givenHero].mp} MP!`);
+        } else {
+          console.log(`${givenHero} does not have enough MP to cast ${spellName}!`);
+        }
+
         break;
     }
-
-    match = input.shift();
+    match = input.shift().split(` - `);
   }
 
   for (let heroName in heroes) {
     console.log(`${heroName}\n  HP: ${heroes[heroName].hp}\n  MP: ${heroes[heroName].mp}`);
   }
+
 }
 
-solve([
-  '2',
-  'Solmyr 85 120',
-  'Kyrre 99 50',
-  'Heal - Solmyr - 10',
-  'Recharge - Solmyr - 50',
-  'TakeDamage - Kyrre - 66 - Orc',
-  'CastSpell - Kyrre - 15 - ViewEarth',
-  'End'
-]);
-
 // solve([
-//     `4`,
-//     `Adela 90 150`,
-//     `SirMullich 70 40`,
-//     `Ivor 1 111`,
-//     `Tyris 94 61`,
-//     `Heal - SirMullich - 50`,
-//     `Recharge - Adela - 100`,
-//     `CastSpell - Tyris - 1000 -
-// Fireball`,
-//     `TakeDamage - Tyris - 99 - Fireball`,
-//     `TakeDamage - Ivor - 3 - Mosquito`,
-//     `End`
+//   '2',
+//   'Solmyr 85 120',
+//   'Kyrre 99 50',
+//   'Heal - Solmyr - 10',
+//   'Recharge - Solmyr - 50',
+//   'TakeDamage - Kyrre - 66 - Orc',
+//   'CastSpell - Kyrre - 15 - ViewEarth',
+//   'End'
 // ]);
+
+solve([
+  `4`,
+  `Adela 90 150`,
+  `SirMullich 70 40`,
+  `Ivor 1 111`,
+  `Tyris 94 61`,
+  `Heal - SirMullich - 50`,
+  `Recharge - Adela - 100`,
+  `CastSpell - Tyris - 1000 - Fireball`,
+  `TakeDamage - Tyris - 99 - Fireball`,
+  `TakeDamage - Ivor - 3 - Mosquito`,
+  `End`
+]);
