38f6163ae4d5700eb9a53a9c87ac3fc08a03dda0
peaceGloriole <dmtrpwlw@icloud.com>
2023-11-26 20:58:53 +0200

    83/100 some edge case probably

---
+++ worldTour.js
@@ -0,0 +1,54 @@
+function worldTour(arr) {
+  let stop = arr.shift();
+
+  let actions = arr.shift().split(`:`);
+  let command = actions[0];
+
+  while (command != `Travel`) {
+
+    if (command == `Add Stop`) {
+      let index = Number(actions[1]);
+      let newDestination = actions[2];
+
+      if (index >= 0) {
+        let left = stop.slice(0, index);
+        let right = stop.slice(index);
+        stop = left + newDestination + right;
+      }
+
+      console.log(stop);
+    } else if (command == `Remove Stop`) {
+      let startIndex = Number(actions[1]);
+      let endIndex = Number(actions[2]);
+      let remove = stop.slice(startIndex, endIndex + 1);
+
+      if (startIndex >= 0 && endIndex >= 0 && stop.includes(remove)) {
+        stop = stop.replace(remove, ``);
+      }
+
+      console.log(stop);
+    } else if (command == `Switch`) {
+      let oldStr = actions[1];
+      let newStr = actions[2];
+
+      if (stop.includes(oldStr)) {
+        stop = stop.replace(oldStr, newStr);
+      }
+
+      console.log(stop);
+    }
+
+    actions = arr.shift().split(`:`);
+    command = actions[0];
+  }
+
+  console.log(`Ready for world tour! Planned stops: ${stop}`);
+}
+
+worldTour([
+  "Hawai::Cyprys-Greece",
+  "Add Stop:7:Rome",
+  "Remove Stop:11:16",
+  "Switch:Hawai:Bulgaria",
+  "Travel"
+]);
